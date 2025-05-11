89eb9fdb10a10efa662eb6a870957184a14be9d2
Endrei <94779230+EnderTheDeveloper@users.noreply.github.com>
2025-05-11 23:32:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const CLEAR-KEY = process.env.CLEAR_KEY;
+const CLEAR_KEY = process.env.CLEAR_KEY;
 app.use(cors());
 app.use(express.json());
 
